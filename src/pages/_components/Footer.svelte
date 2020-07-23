41c22557706d6a1faf9f13bf11f31ba1b87a2665
jbixon13 <jbixon13@gmail.com>
2020-07-23 00:12:17 -0400

    add style to footer component

--- src/pages/_components/Footer.svelte
+++ src/pages/_components/Footer.svelte
@@ -10,4 +10,18 @@
 </main>
 
 <style>
+    footer {
+        display: block;
+        color: var(--footer-text);
+        font-weight: 300;
+        padding-top: 1vh; 
+        padding-bottom: 1vh;
+        border-top: 1px solid var(--line-color);
+        background-color: var(--primary-color);
+        text-align: center;
+    }
+
+    p {
+        margin: unset;
+    }
 </style>
