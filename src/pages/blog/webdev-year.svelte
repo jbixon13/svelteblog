9b1dece3c0d9c9362e1a39020d52b192bd53b987
jbixon13 <jbixon13@gmail.com>
2020-07-30 23:34:52 -0400

    add ArticleContainer and globally scope styles to it in articles

--- src/pages/blog/webdev-year.svelte
+++ src/pages/blog/webdev-year.svelte
@@ -1,8 +1,24 @@
 <script>
+    import Header from '../_components/Header.svelte'
+    import Footer from '../_components/Footer.svelte'
+    import ArticleContainer from './_components/ArticleContainer.svelte'
+    import { articles } from '../../stores.js'
+    
+    let article = $articles[0];
 </script>
 
 <main>
-    <p>Hello</p>
+    <Header />
+    <ArticleContainer>
+        <div class='article'>
+            <div class='article-header'>
+                <h1 class='title'>{article.title}</h1>
+                <p class='subtitle'>{article.subtitle}</p>
+            </div>
+            <p>Normal article text</p>
+        </div>
+    </ArticleContainer>
+    <Footer />
 </main>
 
 <style>
