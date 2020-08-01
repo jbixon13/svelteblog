f6497ea27bf5b72163e6780ebf2338b2de1a3031
jbixon13 <jbixon13@gmail.com>
2020-08-01 15:33:49 -0400

    add contact component and style

---
+++ src/pages/contact.svelte
@@ -0,0 +1,42 @@
+<main>
+    <ul>
+        <li>
+            <a href='mailto:jbixon13@gmail.com'>Mail</a>
+        </li>
+        <li>
+            <a href='https://www.github.com/jbixon13'>Github</a>
+        </li>
+        <li>
+            <a href='https://www.twitter.com/bixondata'>Twitter</a>
+        </li>
+        <li>
+            <a href='https://www.linkedin.com/in/jasonbixon'>LinkedIn</a>
+        </li>
+    </ul>
+</main>
+
+<style>
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        text-align: center;
+        margin-top: 32vh;
+        margin-bottom: 32vh;
+        list-style: none;
+    }
+
+    li {
+        flex: 1 0 21%;
+    }
+
+    img {
+        width:30px;
+    }
+
+    @media (min-width: 768px) {
+        ul {
+            margin-top: 40vh;
+            margin-bottom: 40vh;
+        }
+    }
+</style>
